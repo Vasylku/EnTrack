e0922fefb768fefdf7f15c1df32cd70674f45d79
Vasylku <vasylkuvayskov@gmail.com>
2023-02-22 20:44:41 -0600

    expo_and minorFixes

--- SP23.P03.Web/ClientApp/src/App.js
+++ SP23.P03.Web/ClientApp/src/App.js
@@ -1,14 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {  useState,useEffect } from 'react';
 
+
+function MyList(){
+  const[getStations,setStations] = useState([]);
+  useEffect(()=>{
+    fetch("/api/stations")
+    .then((x) => x.json())
+    .then((x) => {
+    setStations(x);
+    }); 
+  },[]);
+  return (
+    <div>
+  <ul>     
+    {getStations.map((x) => (
+      <li key= {x.id}> {x.name}{""}
+      </li>
+    ))}
+   </ul>
+   </div>
+  );
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        
         </p>
+      <MyList/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +43,7 @@ function App() {
           Learn React
         </a>
       </header>
+     
     </div>
   );
 }
