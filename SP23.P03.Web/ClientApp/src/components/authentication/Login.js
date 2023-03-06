9864551c88a1218a6a6f5ade78015386d5ace135
Vasylku <vasylkuvayskov@gmail.com>
2023-03-06 10:53:21 -0600

    logo and name

--- SP23.P03.Web/ClientApp/src/components/authentication/Login.js
+++ SP23.P03.Web/ClientApp/src/components/authentication/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LoginPage =() =>{
     return (
         <main className="mx-auto flex min-h-screen w-full items-center justify-center bg-gray-900 text-white">
-            <section className="flex w-[30rem] flex-col space-y-10">
+            <section className="flex w-[30rem] flex-col space-y-10 blue-glassmorphism p-12">
                 <div className="text-center text-4xl font-medium">Log In</div>
 
                 <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-indigo-500">
@@ -22,7 +22,7 @@ const LoginPage =() =>{
                     />
                 </div>
 
-                <button className="transform rounded-sm bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-400">
+                <button className="transform rounded-full bg-yellow-700 py-2 px-7 mx-auto font-bold duration-300 hover:bg-yellow-400">
                     LOG IN
                 </button>
 
